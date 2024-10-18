9bd77b8d191a3ff5b17025c87e82d38108584bd1
Mr Whipp <dwhipp88@gmail.com>
2024-10-18 17:56:02 -0600

    added missing files, working on logic for post route, making progress

--- index.js
+++ index.js
@@ -18,19 +18,12 @@ db.query('SELECT * FROM visited_countries', (err, res) => {
   if (err) {
     console.log('Error executing query', err.stack);
   } else {
-    console.log('DB ROWS', res.rows);
-
     res.rows.forEach(function (code) {
-      console.log('forEach: ', code.country_code);
       data.push(code.country_code);
     });
 
     dbRowLength = res.rows.length;
     countryCode = res.rows.country_code;
-    // data = res.rows.length;
-    console.log('COUNTRY CODE: ', data);
-    console.log('Variables: ' + dbRowLength + ' : ' + data);
-    // console.log('DATA: ', data);
   }
 });
 
@@ -43,11 +36,46 @@ app.use(express.static('public'));
 
 app.get('/', async (req, res) => {
   //Write your code here.
-  console.log('ROOT IS LISTENING.');
   res.render('index.ejs', {
     total: dbRowLength,
     countries: data,
   });
+});
+
+app.post('/add', async (req, res) => {
+  try {
+    const userInput = req.body.country.toLowerCase();
+    const dbCheck = await db.query('SELECT * FROM countries');
+
+    dbCheck.rows.forEach(function (row) {
+      if (
+        userInput === row.country_name.toLowerCase() ||
+        userInput === row.country_code.toLowerCase()
+      ) {
+        console.log('PRE DATA:', data);
+        if (data.includes(row.country_code)) {
+          console.log('Present');
+          // return;
+        } else {
+          console.log('NOT present');
+          data.push(row.country_code);
+          dbRowLength = dbRowLength + 1;
+        }
+        console.log('POST DATA: ', data);
+      }
+      // res.redirect(`http://localhost:${port}`);
+    });
+
+    // res.redirect(req.get('referer'));
+    console.log('ROW LENGTH: ', dbRowLength);
+    console.log('DATA: ', data);
+    res.render('index.ejs', {
+      total: dbRowLength,
+      countries: data,
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Error with request' });
+  }
 });
 
 app.listen(port, () => {
