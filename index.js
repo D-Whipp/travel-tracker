ef0d0592f0b8bb4470143290260a08918474327e
Mr Whipp <dwhipp88@gmail.com>
2024-10-25 16:52:12 -0600

    setup post route so user can now save entries into db, also comparing code snippets to sniff out why the ui doesnt update with the db data

--- index.js
+++ index.js
@@ -34,12 +34,49 @@ let data = [];
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// app.get('/', async (req, res) => {
+//Write your code here.
+// const response = db.query(
+//   'SELECT * FROM visited_countries',
+//   (err, res) => {
+//     if (err) {
+//       console.log('Error executing query', err.stack);
+//     } else {
+//       res.rows.forEach(function (row) {
+//         data.push(row.country_code);
+//       });
+
+//       dbRowLength = res.rows.length;
+//       // countryCode = res.rows; // returns undefined
+
+//       console.log('ROW LENGTH: ', dbRowLength);
+//       // console.log('COUNTRY CODE: ', countryCode);
+//       console.log('Data: ', data);
+//     }
+//   }
+// );
+
+//   res.render('index.ejs', {
+//     total: dbRowLength,
+//     countries: data,
+//   });
+// });
+
+// GET home page
 app.get('/', async (req, res) => {
-  //Write your code here.
-  res.render('index.ejs', {
-    total: dbRowLength,
-    countries: data,
+  const result = await db.query(
+    'SELECT country_code FROM visited_countries'
+  );
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
   });
+  console.log(result.rows);
+  res.render('index.ejs', {
+    countries: countries,
+    total: countries.length,
+  });
+  db.end();
 });
 
 app.post('/add', async (req, res) => {
@@ -58,21 +95,31 @@ app.post('/add', async (req, res) => {
           // return;
         } else {
           console.log('NOT present');
+          db.query(
+            'INSERT INTO visited_countries (country_code) VALUES($1)',
+            [row.country_code]
+          );
           data.push(row.country_code);
           dbRowLength = dbRowLength + 1;
         }
         console.log('POST DATA: ', data);
       }
+      // res.render('index.ejs', {
+      //   total: dbRowLength,
+      //   countries: data,
+      // });
+
       // res.redirect(`http://localhost:${port}`);
     });
 
-    // res.redirect(req.get('referer'));
+    res.redirect(req.get('referer'));
     console.log('ROW LENGTH: ', dbRowLength);
     console.log('DATA: ', data);
-    res.render('index.ejs', {
-      total: dbRowLength,
-      countries: data,
-    });
+    // db.end();
+    // res.render('index.ejs', {
+    //   total: dbRowLength,
+    //   countries: data,
+    // });
   } catch (error) {
     res.status(500).json({ message: 'Error with request' });
   }
