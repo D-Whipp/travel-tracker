af8272f018ba548be97fece62371f59968f4ff55
Mr Whipp <dwhipp88@gmail.com>
2024-10-28 15:11:31 -0600

    gave it my best attempt but im moving too slowly. im now going through the instructors explanation for the db challenges

--- index.js
+++ index.js
@@ -11,50 +11,51 @@ const db = new pg.Client({
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
+  port: 5432,
 });
 db.connect();
 
-db.query('SELECT * FROM visited_countries', (err, res) => {
-  if (err) {
-    console.log('Error executing query', err.stack);
-  } else {
-    res.rows.forEach(function (code) {
-      data.push(code.country_code);
-    });
+// db.query('SELECT * FROM visited_countries', (err, res) => {
+//   if (err) {
+//     console.log('Error executing query', err.stack);
+//   } else {
+//     res.rows.forEach(function (code) {
+//       data.push(code.country_code);
+//     });
 
-    dbRowLength = res.rows.length;
-    countryCode = res.rows.country_code;
-  }
-});
+//     dbRowLength = res.rows.length;
+//     countryCode = res.rows.country_code;
+//   }
+// });
 
-let dbRowLength;
-let countryCode = [];
-let data = [];
+// let dbRowLength;
+// let countryCode = [];
+// let data = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // app.get('/', async (req, res) => {
-//Write your code here.
-// const response = db.query(
-//   'SELECT * FROM visited_countries',
-//   (err, res) => {
-//     if (err) {
-//       console.log('Error executing query', err.stack);
-//     } else {
-//       res.rows.forEach(function (row) {
-//         data.push(row.country_code);
-//       });
+//   //Write your code here.
+//   // const response = db.query(
+//   //   'SELECT * FROM visited_countries',
+//   //   (err, res) => {
+//   //     if (err) {
+//   //       console.log('Error executing query', err.stack);
+//   //     } else {
+//   //       res.rows.forEach(function (row) {
+//   //         data.push(row.country_code);
+//   //       });
 
-//       dbRowLength = res.rows.length;
-//       // countryCode = res.rows; // returns undefined
+//   //       dbRowLength = res.rows.length;
+//   //       // countryCode = res.rows; // returns undefined
 
-//       console.log('ROW LENGTH: ', dbRowLength);
-//       // console.log('COUNTRY CODE: ', countryCode);
-//       console.log('Data: ', data);
-//     }
-//   }
-// );
+//   //       console.log('ROW LENGTH: ', dbRowLength);
+//   //       // console.log('COUNTRY CODE: ', countryCode);
+//   //       console.log('Data: ', data);
+//   //     }
+//   //   }
+//   // );
 
 //   res.render('index.ejs', {
 //     total: dbRowLength,
@@ -79,51 +80,47 @@ app.get('/', async (req, res) => {
   db.end();
 });
 
-app.post('/add', async (req, res) => {
-  try {
-    const userInput = req.body.country.toLowerCase();
-    const dbCheck = await db.query('SELECT * FROM countries');
+// app.post('/add', async (req, res) => {
+//   db.connect();
+//   try {
+//     const userInput = req.body.country.toLowerCase();
+//     const dbCheck = await db.query('SELECT * FROM countries');
 
-    dbCheck.rows.forEach(function (row) {
-      if (
-        userInput === row.country_name.toLowerCase() ||
-        userInput === row.country_code.toLowerCase()
-      ) {
-        console.log('PRE DATA:', data);
-        if (data.includes(row.country_code)) {
-          console.log('Present');
-          // return;
-        } else {
-          console.log('NOT present');
-          db.query(
-            'INSERT INTO visited_countries (country_code) VALUES($1)',
-            [row.country_code]
-          );
-          data.push(row.country_code);
-          dbRowLength = dbRowLength + 1;
-        }
-        console.log('POST DATA: ', data);
-      }
-      // res.render('index.ejs', {
-      //   total: dbRowLength,
-      //   countries: data,
-      // });
+//     dbCheck.rows.forEach(function (row) {
+//       if (
+//         userInput === row.country_name.toLowerCase() ||
+//         userInput === row.country_code.toLowerCase()
+//       ) {
+//         if (data.includes(row.country_code)) {
+//           console.log('Present');
+//           // return;
+//         } else {
+//           console.log('NOT present');
+//           db.query(
+//             'INSERT INTO visited_countries (country_code) VALUES($1)',
+//             [row.country_code]
+//           );
+//           data.push(row.country_code);
+//           dbRowLength = dbRowLength + 1;
+//         }
+//         console.log('POST DATA: ', data);
+//       }
+//     });
 
-      // res.redirect(`http://localhost:${port}`);
-    });
+//     // res.redirect(req.get('referer'));
+//     console.log('ROW LENGTH: ', dbRowLength);
+//     console.log('DATA: ', data);
+//     // db.end();
+//     // res.render('index.ejs', {
+//     //   total: dbRowLength,
+//     //   countries: data,
+//     // });
+//   } catch (error) {
+//     res.status(500).json({ message: 'Error with request' });
+//   }
 
-    res.redirect(req.get('referer'));
-    console.log('ROW LENGTH: ', dbRowLength);
-    console.log('DATA: ', data);
-    // db.end();
-    // res.render('index.ejs', {
-    //   total: dbRowLength,
-    //   countries: data,
-    // });
-  } catch (error) {
-    res.status(500).json({ message: 'Error with request' });
-  }
-});
+//   // db.end();
+// });
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
